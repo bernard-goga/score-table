731d696186fcd9707bd8a743a55c90ea64dae93d
Bernard <gogabernard@gmail.com>
2021-02-28 23:09:05 +0100

    first changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+import BodyWrapper from './components/bodywrapper';
+import Playercard from './components/playercard';
+import PlayerList from './components/playerlist';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+    <BodyWrapper>
+      <PlayerList />
+    </BodyWrapper>
     </div>
   );
 }
